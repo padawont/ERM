1eb6d67d8d56678bb86c5cdb98aa94ead640d2cb
ryan harris zubair <ryanharriszubair@gmail.com>
2025-04-14 19:59:17 +0200

    added the models

---
+++ finance/protobuf/target/debug/deps/protobuf-0e5b11ad804c6e47.d
@@ -0,0 +1,5 @@
+/home/ryanhz/Projects/ERM/finance/protobuf/target/debug/deps/libprotobuf-0e5b11ad804c6e47.rmeta: src/lib.rs
+
+/home/ryanhz/Projects/ERM/finance/protobuf/target/debug/deps/protobuf-0e5b11ad804c6e47.d: src/lib.rs
+
+src/lib.rs:
